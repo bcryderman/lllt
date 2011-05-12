d9852a76f23bb3a26f6c5b3b6f76a773bee4b230
Preston Winfrey <winfrey.preston@gmail.com>
2011-05-11 20:59:49 -0500

    All new public changes

--- public/js/script.js
+++ public/js/script.js
@@ -6,8 +6,8 @@ $(function() {
 	$('.decimal').numeric({ allow: '.' });
 	$('table tr:odd').css('background-color', '#cef6e3');
 	$('.phone').mask('(999) 999-9999');
-	$('.datepicker').datepicker();
-	$('.timepicker').timepicker();
+	$('.datepicker').mask('99/99/9999').datepicker();
+	$('.timepicker').mask('99:99').timepicker();
 });
 
 //var emailRegEx = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
